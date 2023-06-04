8e31b6ff7a921da84ff89788cdc180c4ba6011e2
Anrii Sereda <102906498+SeredaAndrey@users.noreply.github.com>
2023-06-04 23:08:34 +0300

    Update generatorValidate.js

--- validate/generatorValidate.js
+++ validate/generatorValidate.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const generatorSchema = Joi.object({
   brand: Joi.string().max(15).min(1),
   type: Joi.string().max(15).min(1),
-  priceOfOfirstChangeOilReglamentil: Joi.number().positive(),
+  firstChangeOilReglament: Joi.number().positive(),
   nextChangeOilReglament: Joi.number().positive(),
   electricalPower: Joi.number().positive(),
   dataFirstStart: Joi.number().positive(),
