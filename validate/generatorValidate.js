18f069fb01fda15dc06e94f1433b0aaa94f688be
Andrii Sereda <a.a.sereda@ukr.net>
2023-06-05 09:05:31 +0300

    correcting validate of generator setting

--- validate/generatorValidate.js
+++ validate/generatorValidate.js
@@ -6,7 +6,7 @@ const generatorSchema = Joi.object({
   firstChangeOilReglament: Joi.number().positive(),
   nextChangeOilReglament: Joi.number().positive(),
   electricalPower: Joi.number().positive(),
-  dataFirstStart: Joi.number().positive(),
+  dataFirstStart: Joi.date(),
   workingFirsStart: Joi.number().positive(),
   oilVolume: Joi.number().positive(),
   fuelVolume: Joi.number().positive(),
