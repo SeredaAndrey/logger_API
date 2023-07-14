fdf660afc87d991e85b27fe9e7c6872e1b6bbf52
Andrii Sereda <a.a.sereda@ukr.net>
2023-07-14 14:51:54 +0300

    add in getCalcDataController creating new calcdata

--- controllers/calcDataController.js
+++ controllers/calcDataController.js
@@ -21,6 +21,7 @@ const {
 
 const getCalcDataController = async (req, res, next) => {
   const ownerId = req.owner._id;
+  console.log("ownerId: ", ownerId);
   const data = await getCalcDataService(ownerId);
   if (data) {
     res.status(200).json({
